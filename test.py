350e4e38695cbd783e2500f30c42f12c209a8d33
tinokingstone <tinokingstone@gmail.com>
2020-02-14 11:47:00 +0000

    commit sdfsdf

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 name = "steve"
 discount = False
+age = 32 
+wallet = 29.95
+
+print (name + str(discount) + str(age) + str(wallet))
+
+firstname = str(input("please enter your first name"))
+secondname = str(input("please enter your second name"))
+welcomemsg = "Hello" + " " + firstname + " " + secondname
+print (welcomemsg)
+
+num1 = input("enter a whole number: ")
+num2 = input("enter a decimal number: ")
+
+integer_num = int(num1)
+float_num = float(num2)
+round_num = int(round(num1))
+
+print (nfloat_numum2)
+print (round_num)
+print (integer_num)
+
